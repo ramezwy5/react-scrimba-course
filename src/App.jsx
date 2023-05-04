4dcb5a8d0e139df5496d65db55446a2d68455e94
Kaaper <ramez4fx@gmail.com>
2023-05-04 18:08:53 -0400

    map

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+// components
+import Navbar from '../public/components/Navbar';
+import Footer from '../public/components/Footer';
+import Card from '../public/components/Card';
+import Hero from '../public/components/Hero';
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Hero/>
+      <Card  />
+      <Footer />
+    </>
+  )
+}
+
+export default App
