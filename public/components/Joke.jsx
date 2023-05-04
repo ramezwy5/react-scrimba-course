72d1beba16b9ad6d83cbd79e43f44af0822beadf
Kaaper <ramez4fx@gmail.com>
2023-05-04 15:14:01 -0400

    Array.prototype.map()

--- public/components/Joke.jsx
+++ public/components/Joke.jsx
@@ -1,16 +1,30 @@
 // propTypes
 
 import PropTypes from "prop-types"
+import jokes from './jokesData'
 
 export default function Joke(props) {
     Joke.propTypes = {
         setup: PropTypes.string.isRequired,
         punchline: PropTypes.string.isRequired,
+        count: PropTypes.string.isRequired,
     }
+    let count = 1
+    props = jokes.map(joke => {
+        joke.count = count++
+        console.log(joke.count)
+        return (
+            <>
+                {joke.setup && <h3>Setup: {joke.setup}</h3> }
+                <p>Punchline: {joke.punchline}</p>
+            </>
+        )
+        
+    })
+    console.log(props)
     return (
         <div>
-            {props.setup && <h3>Setup: {props.setup}</h3>}
-            <p>Punchline: {props.punchline}</p>
+            {props}
             <hr />
         </div>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,19 @@ import './style.css'
 // import Footer from '../public/components/Footer'
 import Joke from '../public/components/Joke'
 
+
+
 function App() {
+  
   return (
     <div className='container'>
-      {/* <Header /> */}
-      <Joke 
-        punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
-      />
-      <Joke 
-        setup="I got my daughter a fridge for her birthday." 
-        punchline="I can't wait to see her face light up when she opens it." 
-      />
-      <Joke 
-        setup="How did the hacker escape the police?" 
-        punchline="He just ransomware!" 
-      />
-      <Joke 
-        setup="Why don't pirates travel on mountain roads?" 
-        punchline="Scurvy." 
-      />
-      <Joke 
-        setup="Why do bees stay in the hive in the winter?" 
-        punchline="Swarm." 
-      />
-      <Joke 
-        setup="What's the best thing about Switzerland?" 
-        punchline="I don't know, but the flag is a big plus!" 
-      />
-      {/* <Footer /> */}
+      <Joke />
     </div>
   )
 }
+/***
 
+      <Header />
+      <Footer />
+*/
 export default App
